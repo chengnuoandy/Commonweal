3ffb3af725b989d73eefb6482cfcdf03d92ee0f6
HanBigBoss <986656343@qq.com>
2016-06-05 22:05:55 +0800

    test

--- app/src/main/java/com/goldenratio/commonweal/MainActivity.java
+++ app/src/main/java/com/goldenratio/commonweal/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //测试push
+        //韩尚水测试
     }
 }

30f0d9ee0bd20f713fbac51c6e6af40eee840af2
想你时的孤独 <786586063@qq.com>
2016-06-06 20:29:06 +0800

    6.6晚修改完成3

--- app/src/main/java/com/goldenratio/commonweal/ui/activity/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.goldenratio.commonweal.ui.activity;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Administrator on 2016/6/6.
- */
-public class MainActivity extends AppCompatActivity {
-}

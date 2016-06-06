7115b7fb2dd62a7fd18c5a03e05f28c0d0931fd6
Kiuber Z <582279412@qq.com>
2016-06-06 20:03:58 +0800

    优化

---
+++ app/src/main/java/com/goldenratio/commonweal/ui/activity/MainActivity.java
@@ -0,0 +1,9 @@
+package com.goldenratio.commonweal.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/6/6.
+ */
+public class MainActivity extends AppCompatActivity {
+}

53dc7b139324313b8470b76308312c7f3d1c4db6
kiuber <582279412@qq.com>
2016-06-06 23:04:26 +0800

    MainActivity

---
+++ app/src/main/java/com/goldenratio/commonweal/ui/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.goldenratio.commonweal.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/6/6.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

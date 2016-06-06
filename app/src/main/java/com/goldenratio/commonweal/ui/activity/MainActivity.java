25e287b2ec67d337bba8149a0227e0eda9c76427
kiuber <582279412@qq.com>
2016-06-06 18:31:02 +0800

    优化

---
+++ app/src/main/java/com/goldenratio/commonweal/ui/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.goldenratio.commonweal.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.goldenratio.commonweal.R;
+
+/**
+ * Created by Administrator on 2016/6/6.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //sdfasfsaf
+        View view = findViewById(R.id.et_name);
+    }
+}

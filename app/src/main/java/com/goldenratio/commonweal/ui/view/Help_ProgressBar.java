1c101a4ead970811be0213a09f4fcd96898615f3
CharmNight <499715548@qq.com>
2016-06-21 09:42:49 +0800

    项目页面---水平的  字貌似有点小=。=

--- app/src/main/java/com/goldenratio/commonweal/ui/view/Help_ProgressBar.java
+++ app/src/main/java/com/goldenratio/commonweal/ui/view/Help_ProgressBar.java
@@ -1,123 +1,80 @@
 package com.goldenratio.commonweal.ui.view;
 
 
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
+import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.ProgressBar;
 
 /**
  * Created by Administrator on 2016/6/20.
  */
 
-public class Help_ProgressBar extends View {
-    private Paint mBackPaint;
-    private Paint mFrontPaint;
-    private Paint mTextPaint;
-    private float mStrokeWidth = 50;
-    private float mHalfStrokeWidth = mStrokeWidth / 2;
-    private float mRadius = 200;
-    private RectF mRect;
-    private int mProgress = 0;
-    //目标值，想改多少就改多少
-    private int mTargetProgress = 90;
-    private int mMax = 100;
-    private int mWidth;
-    private int mHeight;
-
+public class Help_ProgressBar extends ProgressBar {
+    String text;
+    Paint mPaint;
 
     public Help_ProgressBar(Context context) {
         super(context);
-        init();
+        // TODO Auto-generated constructor stub
+        System.out.println("1");
+        initText();
     }
+
+    public Help_ProgressBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // TODO Auto-generated constructor stub
+        System.out.println("2");
+        initText();
+    }
+
 
     public Help_ProgressBar(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        // TODO Auto-generated constructor stub
+        System.out.println("3");
+        initText();
     }
-
-    public Help_ProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-    }
-
-
-    //完成相关参数初始化
-    private void init() {
-        mBackPaint = new Paint();
-        mBackPaint.setColor(Color.WHITE);
-        mBackPaint.setAntiAlias(true);
-        mBackPaint.setStyle(Paint.Style.STROKE);
-        mBackPaint.setStrokeWidth(mStrokeWidth);
-
-        mFrontPaint = new Paint();
-        mFrontPaint.setColor(Color.GREEN);
-        mFrontPaint.setAntiAlias(true);
-        mFrontPaint.setStyle(Paint.Style.STROKE);
-        mFrontPaint.setStrokeWidth(mStrokeWidth);
-
-
-        mTextPaint = new Paint();
-        mTextPaint.setColor(Color.GREEN);
-        mTextPaint.setAntiAlias(true);
-        mTextPaint.setTextSize(80);
-        mTextPaint.setTextAlign(Paint.Align.CENTER);
-    }
-
-
-    //重写测量大小的onMeasure方法和绘制View的核心方法onDraw()
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        mWidth = getRealSize(widthMeasureSpec);
-        mHeight = getRealSize(heightMeasureSpec);
-        setMeasuredDimension(mWidth, mHeight);
-
-    }
-
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        initRect();
-        float angle = mProgress / (float) mMax * 360;
-        canvas.drawCircle(mWidth / 2, mHeight / 2, mRadius, mBackPaint);
-        canvas.drawArc(mRect, -90, angle, false, mFrontPaint);
-        canvas.drawText(mProgress + "%", mWidth / 2 + mHalfStrokeWidth, mHeight / 2 + mHalfStrokeWidth, mTextPaint);
-        if (mProgress < mTargetProgress) {
-            mProgress += 1;
-            invalidate();
-        }
+    public synchronized void setProgress(int progress) {
+        // TODO Auto-generated method stub
+        setText(progress);
+        super.setProgress(progress);
 
     }
 
-    public int getRealSize(int measureSpec) {
-        int result = 1;
-        int mode = MeasureSpec.getMode(measureSpec);
-        int size = MeasureSpec.getSize(measureSpec);
-
-        if (mode == MeasureSpec.AT_MOST || mode == MeasureSpec.UNSPECIFIED) {
-            //自己计算
-            result = (int) (mRadius * 2 + mStrokeWidth);
-        } else {
-            result = size;
-        }
-
-        return result;
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+        // TODO Auto-generated method stub
+        super.onDraw(canvas);
+        //this.setText();
+        Rect rect = new Rect();
+        this.mPaint.getTextBounds(this.text, 0, this.text.length(), rect);
+        int x = (getWidth() / 2) - rect.centerX();
+        int y = (getHeight() / 2) - rect.centerY();
+        canvas.drawText(this.text, x, y, this.mPaint);
     }
 
-    private void initRect() {
-        if (mRect == null) {
-            mRect = new RectF();
-            int viewSize = (int) (mRadius * 2);
-            int left = (mWidth - viewSize) / 2;
-            int top = (mHeight - viewSize) / 2;
-            int right = left + viewSize;
-            int bottom = top + viewSize;
-            mRect.set(left, top, right, bottom);
-        }
+    //初始化，画笔
+    private void initText(){
+        this.mPaint = new Paint();
+        this.mPaint.setColor(Color.WHITE);
+
+    }
+
+    private void setText(){
+        setText(this.getProgress());
+    }
+
+    //设置文字内容
+    private void setText(int progress){
+        int i = (progress * 100)/this.getMax();
+        this.text = String.valueOf(i) + "%";
     }
 
 }

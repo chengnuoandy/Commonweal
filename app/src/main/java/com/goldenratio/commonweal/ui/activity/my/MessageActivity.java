266ec2f4fb8f7027e27d70467c3a26db4deba1ff
zjty5418 <zjty5418@163.com>
2016-06-26 14:59:08 +0800

    修改包

---
+++ app/src/main/java/com/goldenratio/commonweal/ui/activity/my/MessageActivity.java
@@ -0,0 +1,60 @@
+package com.goldenratio.commonweal.ui.activity.my;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.goldenratio.commonweal.R;
+import com.goldenratio.commonweal.adapter.MyMsgFragmentAdapter;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MessageActivity extends FragmentActivity implements ViewPager.OnPageChangeListener {
+
+    @BindView(R.id.tv_dynamicMessage)
+    TextView mTvDynamicMessage;
+    @BindView(R.id.tv_privateLetter)
+    TextView mTvPrivateLetter;
+    @BindView(R.id.iv_message_tab)
+    ImageView mIvMessageTab;
+    @BindView(R.id.vp_my_message)
+    ViewPager mVpMessage;
+
+    private FragmentPagerAdapter mFragmentPagerAdapter;
+
+    private List<Fragment> mFragmentList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+        ButterKnife.bind(this);
+
+        mFragmentPagerAdapter = new MyMsgFragmentAdapter(getSupportFragmentManager());
+        mVpMessage.setCurrentItem(0);
+        mVpMessage.addOnPageChangeListener(this);
+        mVpMessage.setAdapter(mFragmentPagerAdapter);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+}

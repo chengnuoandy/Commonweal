c391fef8ce35b7d4f93e6d0ae0dd48adc106418e
zjty5418 <zjty5418@163.com>
2016-06-26 12:26:21 +0800

    动态消息后台代码

--- app/src/main/java/com/goldenratio/commonweal/ui/fragment/PrivateLetterFragment.java
+++ app/src/main/java/com/goldenratio/commonweal/ui/fragment/PrivateLetterFragment.java
@@ -1,10 +1,27 @@
 package com.goldenratio.commonweal.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.goldenratio.commonweal.R;
 
 /**
  * 作者：Created by 龙啸天 on 2016/6/25 0025.
  * 邮箱：jxfengmtx@163.com ---17718
+ *
+ * 消息--私信后台代码
  */
 public class PrivateLetterFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my_private_letter, null);
+
+        return view;
+    }
 }

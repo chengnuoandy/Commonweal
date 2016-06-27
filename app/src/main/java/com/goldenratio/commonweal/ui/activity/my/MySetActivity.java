b1b7425c9ccafa25e84a79d5742af63481a59eea
HanBigBoss <986656343@qq.com>
2016-06-27 21:51:50 +0800

    优化

--- app/src/main/java/com/goldenratio/commonweal/ui/activity/my/MySetActivity.java
+++ app/src/main/java/com/goldenratio/commonweal/ui/activity/my/MySetActivity.java
@@ -34,7 +34,6 @@ public class MySetActivity extends Activity {
 
         isLogin = getIntent().getExtras().getBoolean("islogin");
 
-
     }
 
 
--- app/src/main/java/com/goldenratio/commonweal/ui/fragment/HelpFragment.java
+++ app/src/main/java/com/goldenratio/commonweal/ui/fragment/HelpFragment.java
@@ -35,7 +35,8 @@ import cn.bmob.v3.listener.SaveListener;
 
 import static java.security.AccessController.getContext;
 
-public class HelpFragment extends Fragment implements AdapterView.OnItemClickListener {
+public class
+HelpFragment extends Fragment implements AdapterView.OnItemClickListener {
     private ViewPager mViewPager;
     private PullToRefreshListView mListView;
     private CirclePageIndicator indicator;

59722154dffa09324b1fbd832469d68c7f346e21
CharmNight <499715548@qq.com>
2016-07-14 21:20:33 +0800

    分享

--- app/src/main/java/com/goldenratio/commonweal/ui/activity/HelpDetailActivity.java
+++ app/src/main/java/com/goldenratio/commonweal/ui/activity/HelpDetailActivity.java
@@ -20,6 +20,7 @@ import com.bumptech.glide.Glide;
 import com.goldenratio.commonweal.R;
 import com.goldenratio.commonweal.bean.Help;
 import com.goldenratio.commonweal.bean.Help_Top;
+import com.goldenratio.commonweal.onekeyshare.OnekeyShare;
 import com.goldenratio.commonweal.ui.fragment.HelpContentFragment;
 import com.squareup.picasso.Picasso;
 
@@ -27,6 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cn.bmob.v3.listener.SaveListener;
+import cn.sharesdk.framework.ShareSDK;
 
 /**
  * Created by Administrator on 2016/6/22.
@@ -98,6 +100,7 @@ public class HelpDetailActivity extends Activity implements View.OnClickListener
         findViewById(R.id.btn_speak).setOnClickListener(this);
     }
 
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -112,6 +115,7 @@ public class HelpDetailActivity extends Activity implements View.OnClickListener
 
             //分享
             case R.id.iv_share:
+                showShare();
                 break;
 
             //评论
@@ -135,5 +139,35 @@ public class HelpDetailActivity extends Activity implements View.OnClickListener
 
                 break;
         }
+    }
+
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.ssdk_oks_share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
     }
 }
--- app/src/main/java/com/goldenratio/commonweal/ui/activity/HelpDonateActivity.java
+++ app/src/main/java/com/goldenratio/commonweal/ui/activity/HelpDonateActivity.java
@@ -2,6 +2,7 @@ package com.goldenratio.commonweal.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.goldenratio.commonweal.R;
 
@@ -10,10 +11,24 @@ import com.goldenratio.commonweal.R;
  * 捐赠时弹出的页面
  */
 
-public class HelpDonateActivity extends Activity {
+public class HelpDonateActivity extends Activity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_donate);
+    }
+
+    private void initView(){
+        findViewById(R.id.iv_back).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            //返回
+            case R.id.iv_back:
+                finish();
+                break;
+        }
     }
 }

1121f263f602722058ce977f8cbf92c5a66d39b5
jxfengmtx <zjty5418@163.com>
2016-08-08 15:26:16 +0800

    实现地址三级联动

--- app/src/main/java/com/goldenratio/commonweal/ui/activity/my/EditAddressActivity.java
+++ app/src/main/java/com/goldenratio/commonweal/ui/activity/my/EditAddressActivity.java
@@ -2,6 +2,7 @@ package com.goldenratio.commonweal.ui.activity.my;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import com.goldenratio.commonweal.R;
 import com.goldenratio.commonweal.bean.Address;
+import com.lljjcoder.citypickerview.widget.CityPickerView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,10 +30,6 @@ public class EditAddressActivity extends Activity {
     TextView mTvEditAddress;
     @BindView(R.id.rl_edit_address)
     RelativeLayout mRlEditAddress;
-    @BindView(R.id.tv_edit_street)
-    TextView mTvEditStreet;
-    @BindView(R.id.rl_place_street)
-    RelativeLayout mRlPlaceStreet;
     @BindView(R.id.et_detail_address)
     EditText mEtDetailAddress;
     @BindView(R.id.btn_save_address)
@@ -46,17 +44,14 @@ public class EditAddressActivity extends Activity {
 
     }
 
-    @OnClick({R.id.iv_edit_address_back, R.id.rl_edit_address, R.id.tv_edit_street, R.id.rl_place_street, R.id.et_detail_address, R.id.btn_save_address})
+    @OnClick({R.id.iv_edit_address_back, R.id.rl_edit_address, R.id.et_detail_address, R.id.btn_save_address})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.iv_edit_address_back:
                 finish();
                 break;
             case R.id.rl_edit_address:
-                break;
-            case R.id.tv_edit_street:
-                break;
-            case R.id.rl_place_street:
+                configCityPickerView();
                 break;
             case R.id.et_detail_address:
                 break;
@@ -71,7 +66,7 @@ public class EditAddressActivity extends Activity {
         String consignee = mEtConsignee.getText().toString();
         String consigneePhone = mEtEditPhone.getText().toString();
         String consigneeAddress = mTvEditAddress.getText() + " "
-                + mTvEditStreet.getText() + " " + mEtDetailAddress.getText();
+                + mEtDetailAddress.getText();
         Address address = new Address();
         address.setConsignee(consignee);
         address.setConsigneePhone(consigneePhone);
@@ -79,5 +74,30 @@ public class EditAddressActivity extends Activity {
         Intent intent = new Intent();
         intent.putExtra("address", address);
         setResult(RESULT_OK, intent);
+    }
+
+    private void configCityPickerView() {
+        CityPickerView cityPickerView = new CityPickerView(EditAddressActivity.this);
+        cityPickerView.setTextColor(Color.BLUE);
+        cityPickerView.setTextSize(20);
+        cityPickerView.setVisibleItems(8);  //新增滚轮内容可见数量
+        cityPickerView.setIsCyclic(true);   //滚轮是否循环滚动
+        cityPickerView.show();
+        cityPickerView.setOnCityItemClickListener(new CityPickerView.OnCityItemClickListener() {
+            @Override
+            public void onSelected(String... citySelected) {
+                //省份
+                String province = citySelected[0];
+                //城市
+                String city = citySelected[1];
+                //区县
+                String district = citySelected[2];
+                //邮编
+                String code = citySelected[3];
+
+                String addre = province + " " + city + " " + district + " ";
+                mTvEditAddress.setText(addre);
+            }
+        });
     }
 }
